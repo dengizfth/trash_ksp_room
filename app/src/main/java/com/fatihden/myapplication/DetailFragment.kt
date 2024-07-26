471dcec3ef397906b08a6baf9450c32707f38f85
Fatih Dengiz <dengizfth@gmail.com>
2024-07-26 15:36:44 +0300

    viewBinding added

--- app/src/main/java/com/fatihden/myapplication/DetailFragment.kt
+++ app/src/main/java/com/fatihden/myapplication/DetailFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.fatihden.myapplication.databinding.FragmentDetailBinding
 
 
 class DetailFragment : Fragment() {
+    private var _binding:FragmentDetailBinding? = null
+    private val binding = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +22,17 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        /*return inflater.inflate(R.layout.fragment_detail, container, false)*/
+        _binding = FragmentDetailBinding.inflate(inflater,container,false)
+
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        _binding = null
     }
 
 
