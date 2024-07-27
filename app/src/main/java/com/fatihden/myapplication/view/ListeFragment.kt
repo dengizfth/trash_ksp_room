33e1011f22e5543cd40f8b67c75f374dc0642773
Fatih Dengiz <dengizfth@gmail.com>
2024-07-27 17:40:09 +0300

    create Database and DAO

--- app/src/main/java/com/fatihden/myapplication/view/ListeFragment.kt
+++ app/src/main/java/com/fatihden/myapplication/view/ListeFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.room.Room
 import com.fatihden.myapplication.databinding.FragmentListeBinding
+import com.fatihden.myapplication.db.DetailDAO
+import com.fatihden.myapplication.db.DetailDatabase
 
 
 class ListeFragment : Fragment() {
@@ -15,8 +18,17 @@ class ListeFragment : Fragment() {
 
     private val binding get() = _binding!!
 
+
+    // Database :
+    private lateinit var db : DetailDatabase
+    private lateinit var detailDAO: DetailDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Database
+        db = Room.databaseBuilder(requireContext(),DetailDatabase::class.java,"Details").build()
+        detailDAO = db.detailDao()
 
     }
 
