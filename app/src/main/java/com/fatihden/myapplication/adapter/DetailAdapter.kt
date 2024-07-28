7fb84288e8c801ee8640f2f8670625b857396ab9
Fatih Dengiz <dengizfth@gmail.com>
2024-07-28 23:54:52 +0300

    DetailAdapter  implementation

--- app/src/main/java/com/fatihden/myapplication/adapter/DetailAdapter.kt
+++ app/src/main/java/com/fatihden/myapplication/adapter/DetailAdapter.kt
@@ -1,5 +1,6 @@
 package com.fatihden.myapplication.adapter
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.fatihden.myapplication.databinding.CardViewBinding
 
@@ -13,6 +14,17 @@ class DetailAdapter : RecyclerView.Adapter<DetailAdapter.DetailHolder>() {
 
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: DetailHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 
 
 }
